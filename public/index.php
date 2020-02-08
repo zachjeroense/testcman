8911cd8ee79589fcb28ab4b1a1429a5fb9535456
zachjeroense <60322547+zachjeroense@users.noreply.github.com>
2020-02-08 12:46:42 +0100

    fix vendor

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ALL);
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
@@ -21,7 +23,7 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'\..\vendor\autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +37,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'\..\bootstrap\app.php';
 
 /*
 |--------------------------------------------------------------------------
